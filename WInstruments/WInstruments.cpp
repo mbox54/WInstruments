c2b6b8fbe0d34c09dd29ff7809d5403b928f879b
Korchagin <Korchagin@KORCHAGIN-PC>
2017-04-25 17:58:18 +0700

    place 2x vector

--- WInstruments/WInstruments.cpp
+++ WInstruments/WInstruments.cpp
@@ -2,11 +2,23 @@
 //
 
 #include "stdafx.h"
+#include <vector>
 
+using namespace std;
+
+typedef int T;
 
 int main()
 {
 	// 123
+
+	int n = 10;
+	int m = 10;
+
+	vector<vector<T> > a(n, vector<T>(m, 1)); // два размера
+
+	// Wait, press key
+	getchar();
 
     return 0;
 }
